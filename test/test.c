77bec88c5b9f2a3cb812f2bd8d45f2c22431a35e
emilenormand <emile.normand.1@ens.etsmtl.ca>
2021-05-17 21:11:37 -0400

    seems to be working

--- test/test.c
+++ test/test.c
@@ -2,7 +2,7 @@
 
 
 #include "unity/unity.h"
-#include "solution_c_bas_niveau.c"
+#include "../src/solution_c_bas_niveau.c"
 
 void setUp(void) {
     // set stuff up here
@@ -13,7 +13,11 @@ void tearDown(void) {
 }
 
 void test_function_should_doBlahAndBlah(void) {
-    //test stuff
+    char c[] = "hello";
+    TEST_ASSERT_EQUAL(8,strlen(c));
+    TEST_FAIL();
+    TEST_FAIL_MESSAGE("message");
+
 }
 
 void test_function_should_doAlsoDoBlah(void) {
